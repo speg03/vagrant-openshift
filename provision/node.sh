6f0fa60cfcf113dcbdd8b7b57af3eb1f3358f27f
Takahiro Yano <speg03@gmail.com>
2015-11-28 14:36:05 +0900

    Remove node

--- provision/node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Network configuration
-nmcli general hostname node.internal
-nmcli connection add type ethernet con-name eth1 ifname eth1
-nmcli connection modify eth1 ipv4.addresses 192.168.133.11/24
-nmcli connection modify eth1 ipv4.method manual
-nmcli connection modify eth1 ipv4.dns 192.168.133.10,8.8.8.8,8.8.4.4
-nmcli connection down eth1 && nmcli connection up eth1
